c4fed8cf2a0afb70ef45ad4d56b8bf1221ee43ec
danguard96 <56619650+danguard96@users.noreply.github.com>
2025-05-10 15:55:58 -0500

    Added spdlog and shadder sources to cmakelists

---
+++ src/precomp.h
@@ -0,0 +1,18 @@
+//
+// Created by Yibuz Pokopodrozo on 2025-05-10.
+//
+
+#pragma once
+
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+#include <cmath>
+#include <cstdlib>
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <glm/glm.hpp>
+#include <functional>
+#include <iostream>
+#include <spdlog/spdlog.h>
+#include <set>
