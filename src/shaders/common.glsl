b9afbd20820ce8d4005472ac4c4a3d6b3bc60e81
danguard96 <56619650+danguard96@users.noreply.github.com>
2025-05-31 22:09:24 -0500

    Removed altered blinn phong

--- src/shaders/common.glsl
+++ src/shaders/common.glsl
@@ -6,17 +6,3 @@ layout(set = 0, binding = 0) uniform UniformTransformations {
     mat4 view;
     mat4 projection;
 } camera;
-
-struct Light {
-    vec4 position;  // w=0 for directional, w=1 for point light
-    vec4 color;     // rgb = color, a = intensity
-    vec4 params;    // x = constant, y = linear, z = quadratic attenuation
-};
-
-#define MAX_LIGHTS 4
-
-layout(set = 0, binding = 1) uniform LightBuffer {
-    vec4 viewPos;        // Camera position in world space
-    uint numLights;      // Number of active lights
-    Light lights[MAX_LIGHTS];
-} lightBuffer;
